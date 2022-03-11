77a71fcb163f3ac116fbe3e547b07764ced94e05
ElenaSlugevich <sa.elena89@gmail.com>
2022-03-11 09:22:02 +0300

    тест

--- src/test/java/JdClassTest.java
+++ src/test/java/JdClassTest.java
@@ -1,2 +1,21 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+
 public class JdClassTest {
+    @Test
+    public static void main(String[] args) {
+        Configuration.holdBrowserOpen = true;
+        open("https://gvm220.lpr.jet.msk.su/");
+
+        element(byName("login")).setValue("jd");
+        element(byName("password")).setValue("q1");
+        $x("//a[.='Войти']").shouldBe(Condition.enabled).click();
+    }
 }
+
