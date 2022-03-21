9dc64240c29e4204dccc2b06cb39b3a6adfcdd74
ElenaSlugevich <sa.elena89@gmail.com>
2022-03-21 18:42:41 +0300

    Авторизация в системе и переход в Объектную модель - Объекты + нажатие на кнопку Создать

--- src/main/java/ru/stqa/pft/LenaProjectAutoTest/FirstClass.java
+++ src/main/java/ru/stqa/pft/LenaProjectAutoTest/FirstClass.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.LenaProjectAutoTest;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World Hello World");
+
     }
 }
 
